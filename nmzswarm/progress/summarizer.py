2caa8a20d59423b0564b7312e3f3d26d0b8237ce
Akihiro Suda <suda.akihiro@lab.ntt.co.jp>
2016-08-30 14:49:43 +0000

    example/etcd: 4 times faster on 5 VMs (second run and later)
    Experimental result:
    
    VM: 4 CPUs, 15 GB RAM
    
    1 VM (`docker run --rm etcd-nmzswarm /NamazuSwarmfile _exec_all`): about 4m
    5 VMs, 80 containers: about 1m + init (pre-compilation time (50s) + gcr push time (50s))
    
    So it is not so fast (just 4m vs 3m) for the first run... :-(

--- nmzswarm/progress/summarizer.py
+++ nmzswarm/progress/summarizer.py
@@ -1,5 +1,6 @@
 """Event sequence summarizer
 """
+import time
 from nmzswarm.progress.event import Event, JobEvent
 
 
@@ -9,16 +10,26 @@ class Summarizer:
     def __init__(self) -> None:
         self.completed = 0
         self.failed = 0
+        self.job_started = {}  # type: Dict[str, float]
+        self.job_took = {}  # type: Dict[str, float]
 
     def handle_event(self, ev: Event) -> None:
         """handles an event"""
         if isinstance(ev, JobEvent):
-            if ev.finished:
+            job_id = ev.command
+            if not ev.finished:
+                self.job_started[job_id] = time.time()
+            else:
+                self.job_took[job_id] = time.time() - self.job_started[job_id]
                 self.completed += 1
                 if ev.code != 0:
                     self.failed += 1
 
     def __str__(self) -> str:
         """summarize the result"""
-        return 'Completed %d jobs (%d failed)' % \
+        s = 'Completed %d jobs (%d failed)\n' % \
             (self.completed, self.failed)
+        for k in sorted(self.job_took, key=self.job_took.get):
+            v = self.job_took[k]
+            s += '%s: took %f seconds (job creation..completion)\n' % (k, v)
+        return s
